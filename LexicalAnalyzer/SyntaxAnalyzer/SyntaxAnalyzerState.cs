ec7edd6c61d4cde8c8a58016a58a16a2370b9619
natisebben <30883874+natisebben@users.noreply.github.com>
2022-07-03 16:48:27 -0300

    Add files via upload

---
+++ LexicalAnalyzer/SyntaxAnalyzer/SyntaxAnalyzerState.cs
@@ -0,0 +1,19 @@
+﻿using Common;
+
+namespace Analyzer
+{
+    public class SyntaxAnalyzerState
+    {
+        public List<Result> Tokens { get; private set; }
+        public Result CurrentToken { get; set; }
+        public int CurrentTokenIndex { get; set; }
+        public int TokensQuantity { get; private set; }
+
+        public SyntaxAnalyzerState(List<Result> tokens)
+        {
+            Tokens = tokens;
+            CurrentTokenIndex = -1;
+            TokensQuantity = tokens.Count();
+        }
+    }
+}
